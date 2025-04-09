7d251af62bda7495ed6fac87d20c4c9014605717
akshay <akshaykatoch38@gmail.com>
2025-04-09 12:21:09 +0530

    [Akshay] MOD:replace the backend url to render url

--- generate-env.js
+++ generate-env.js
@@ -5,7 +5,7 @@ const env = {
   GOOGLE_CLIENT_ID:
     "487636959884-qpcsgvs3m6vcmjtmmt60mnpjb66bv2uj.apps.googleusercontent.com",
   GITHUB_CLIENT_ID: "Ov23liI3VxGwFnrQoeL1",
-  BACKEND_URL: "https://bookstore-backend-p7e1.onrender.com/", // This is the single source of truth
+  API_BASE_URL: process.env.BACKEND_URL || "https://bookstore-backend-p7e1.onrender.com/api/v1" // This is the single source of truth
 };
 
 const content = `window.env = ${JSON.stringify(env, null, 2)};`;
